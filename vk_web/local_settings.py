ab17031b272c63916ae918d21b75584f31b4e964
Egor <des2107@mail.ru>
2023-11-06 10:21:31 +0300

    a

---
+++ vk_web/local_settings.py
@@ -0,0 +1,7 @@
+host="localhost"
+user="root"
+password="rootegor"
+
+DATABASE_NAME = "AskME" # Or path to database file if using sqlite3.
+DATABASE_USER = user # Not used with sqlite3.
+DATABASE_PASSWORD = password # Not used with sqlite3.
