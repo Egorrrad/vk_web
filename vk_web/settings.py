4829358c9f7b02b93d7791e7f1108fd300dfab8f
Egor <des2107@mail.ru>
2023-11-08 16:56:14 +0300

    hfphtityyst

--- vk_web/settings.py
+++ vk_web/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'des2107.fvds.ru']
 
 
 # Application definition
