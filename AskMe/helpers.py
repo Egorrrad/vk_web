a7339fef2314c022b585363bd9a6b7029f117f0f
Egor <des2107@mail.ru>
2023-11-15 09:34:05 +0300

    change save image

--- AskMe/helpers.py
+++ AskMe/helpers.py
@@ -60,6 +60,9 @@ def isEmptyQuestions(request, questionslist, pages_counter):
 def add_image_profile(user, request):
     file = request.FILES['image']
     fs = FileSystemStorage()
+    path = "user_" + str(user.id) + "/avatar." + str(file.name).split(".")[1]
+    if fs.exists(path):
+        fs.delete(path)
     filename = fs.save("user_" + str(user.id) + "/avatar." + str(file.name).split(".")[1], file)
     # file_url = fs.url(filename)
     profile = Profile.objects.get_or_create(user=user)[0]
